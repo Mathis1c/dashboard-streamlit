31ea17ed29458b0058517461345484e274dd5d31
MathisC <he304862@students.ephec.be>
2026-01-09 12:02:03 +0100

    réglage de logique de mode

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.write("")
 st.write("Mode : manu ou auto") 
 mode_choice = st.selectbox("Mode", ["manu", "auto"])
 send_topic = "esp/Manu_Auto_mode"
-send_payload = False if mode_choice == "auto" else True 
+send_payload = True  if mode_choice == "auto" else False #true for auto, false for manu
 if st.button("changement mode"):
     if send_topic == "esp/Manu_Auto_mode" :
         client.publish(send_topic, str(send_payload))  # envoie True/False
